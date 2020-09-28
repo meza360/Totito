2029cabdf56a4013e8131bf9cbb02b8824df051d
Giovani Meza <giovanidav5@outlook.com>
2020-09-28 15:54:54 -0600

    Add project files.

---
+++ Totito/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Totito
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(3, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(113, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 36);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(214, 75);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 36);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(3, 173);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(113, 173);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 35);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(214, 173);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 35);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.AutoSize = true;
+            this.button7.BackColor = System.Drawing.Color.Transparent;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(3, 276);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(94, 35);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.AutoSize = true;
+            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(113, 276);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(94, 35);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.AutoSize = true;
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(214, 276);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(94, 35);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(12, 358);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(107, 21);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(214, 358);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(107, 21);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(320, 387);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Totito/Form1.cs
@@ -0,0 +1,361 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Totito
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Text = "TOTITO";
+
+            
+            radioButton1.Text = "X";
+            radioButton2.Text = "0";
+
+            button1.Text = "";
+            button2.Text = "";
+            button3.Text = "";
+            button4.Text = "";
+            button5.Text = "";
+            button6.Text = "";
+            button7.Text = "";
+            button8.Text = "";
+            button9.Text = "";
+
+            radioButton1.Checked = true;
+
+            MessageBox.Show("Esto es TOTITO, y se juega asi" +
+                "\nCada jugador tendra 4 turnos" +
+                "\nSi alguno de los jugadores logra poner su variable correspondiente en 3 en linea" +
+                "\nGana, de lo contrario perdera" +
+                "\nY si ambos llegan a ocupar sus 4 turnos, y no hay 3 en linea, se considera empate, y se empieza de nuevo" +
+                "\nEmpecemos ☺☺☺");
+  
+        }
+
+        int contx = 0;
+        int conto = 0;
+
+        public void funcion()
+        {
+
+            #region contador
+            //contador de turnos
+            while (contx == 4)
+                {
+                    contx = 4;
+                    if (contx == 4 && conto != 4)
+                    {
+                    radioButton1.Enabled = false;
+                    radioButton2.Checked = true;
+                    
+                    }
+                break;
+                }
+
+            while (conto == 4)
+            {
+                conto = 4;
+                if (conto == 4 && contx !=4)
+                {
+                    radioButton2.Enabled = false;
+                    radioButton1.Checked = true;
+                }
+                break;
+            }
+
+            //contador de turnos
+            #endregion contador
+
+            #region ganax
+            //if de X
+            if (   button1.Text == "x" && button2.Text == "x" && button3.Text == "x"
+                || button1.Text == "x" && button4.Text == "x" && button7.Text == "x" 
+                || button1.Text == "x" && button5.Text == "x" && button9.Text == "x" 
+                || button3.Text == "x" && button5.Text == "x" && button7.Text == "x"
+                || button2.Text == "x" && button5.Text == "x" && button8.Text == "x"
+                || button3.Text == "x" && button6.Text == "x" && button9.Text == "x"
+                || button4.Text == "x" && button5.Text == "x" && button6.Text == "x"
+                || button7.Text == "x" && button8.Text == "x" && button9.Text == "x")
+            {
+                contx = 0;
+                conto = 0;
+                MessageBox.Show("Gano X");
+                button1.Enabled = true;
+                button1.Text = "";
+
+                button2.Enabled = true;
+                button2.Text = "";
+
+                button3.Enabled = true;
+                button3.Text = "";
+
+                button4.Enabled = true;
+                button4.Text = "";
+
+                button5.Enabled = true;
+                button5.Text = "";
+
+                button6.Enabled = true;
+                button6.Text = "";
+
+                button7.Enabled = true;
+                button7.Text = "";
+
+                button8.Enabled = true;
+                button8.Text = "";
+
+                button9.Enabled = true;
+                button9.Text = "";
+
+                radioButton2.Enabled = true;
+                radioButton1.Enabled = true;
+            }
+            #endregion ganax
+
+            #region ganao
+            //if de O
+            if (button1.Text == "o" && button2.Text == "o" && button3.Text == "o"
+                || button1.Text == "o" && button4.Text == "o" && button7.Text == "o"
+                || button1.Text == "o" && button5.Text == "o" && button9.Text == "o"
+                || button3.Text == "o" && button5.Text == "o" && button7.Text == "o"
+                || button2.Text == "o" && button5.Text == "o" && button8.Text == "o"
+                || button3.Text == "o" && button6.Text == "o" && button9.Text == "o"
+                || button4.Text == "o" && button5.Text == "o" && button6.Text == "o"
+                || button7.Text == "o" && button8.Text == "o" && button9.Text == "o")
+            {
+                contx = 0;
+                conto = 0;
+                MessageBox.Show("Gano O");
+                button1.Enabled = true;
+                button1.Text = "";
+
+                button2.Enabled = true;
+                button2.Text = "";
+
+                button3.Enabled = true;
+                button3.Text = "";
+
+                button4.Enabled = true;
+                button4.Text = "";
+
+                button5.Enabled = true;
+                button5.Text = "";
+
+                button6.Enabled = true;
+                button6.Text = "";
+
+                button7.Enabled = true;
+                button7.Text = "";
+
+                button8.Enabled = true;
+                button8.Text = "";
+
+                button9.Enabled = true;
+                button9.Text = "";
+
+                radioButton2.Enabled = true;
+                radioButton1.Enabled = true;
+            }
+            #endregion ganao
+
+            #region empate
+            if (conto == 4 && contx == 4)
+            {
+                MessageBox.Show("Empate, empecemos de nuevo");
+                contx = 0;
+                conto = 0;
+                button1.Enabled = true;
+                button1.Text = "";
+
+                button2.Enabled = true;
+                button2.Text = "";
+
+                button3.Enabled = true;
+                button3.Text = "";
+
+                button4.Enabled = true;
+                button4.Text = "";
+
+                button5.Enabled = true;
+                button5.Text = "";
+
+                button6.Enabled = true;
+                button6.Text = "";
+
+                button7.Enabled = true;
+                button7.Text = "";
+
+                button8.Enabled = true;
+                button8.Text = "";
+
+                button9.Enabled = true;
+                button9.Text = "";
+
+                radioButton2.Enabled = true;
+                radioButton1.Enabled = true;
+            }
+            #endregion empate
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (radioButton1.Checked == true)
+            {
+                button1.Text = "x";
+                contx++;
+                
+            }
+            if (radioButton2.Checked == true)
+            {
+                button1.Text = "o";
+                conto++; 
+            }
+            button1.Enabled = false;
+            funcion();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                button2.Text = "x";
+                contx++;
+            }
+            if (radioButton2.Checked == true)
+            {
+                button2.Text = "o";
+                conto++;
+            }
+            button2.Enabled = false;
+            funcion();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                button3.Text = "x";
+                contx++;
+            }
+            if (radioButton2.Checked == true)
+            {
+                button3.Text = "o";
+                conto++;
+            }
+            button3.Enabled = false;
+            funcion();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                button4.Text = "x";
+                contx++;
+            }
+            if (radioButton2.Checked == true)
+            {
+                button4.Text = "o";
+                conto++;
+            }
+            button4.Enabled = false;
+            funcion();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                button5.Text = "x";
+                contx++;
+            }
+            if (radioButton2.Checked == true)
+            {
+                button5.Text = "o";
+                conto++;
+            }
+            button5.Enabled = false;
+            funcion();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                button6.Text = "x";
+                contx++;
+            }
+            if (radioButton2.Checked == true)
+            {
+                button6.Text = "o";
+                conto++;
+            }
+            button6.Enabled = false;
+            funcion();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                button7.Text = "x";
+                contx++;
+            }
+            if (radioButton2.Checked == true)
+            {
+                button7.Text = "o";
+                conto++;
+            }
+            button7.Enabled = false;
+            funcion();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                button8.Text = "x";
+                contx++;
+            }
+            if (radioButton2.Checked == true)
+            {
+                button8.Text = "o";
+                conto++;
+            }
+            button8.Enabled = false;
+            funcion();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                button9.Text = "x";
+                contx++;
+            }
+            if (radioButton2.Checked == true)
+            {
+                button9.Text = "o";
+                conto++;
+            }
+            button9.Enabled = false;
+            funcion();
+        }
+    }
+}
